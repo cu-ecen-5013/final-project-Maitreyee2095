a9c30c336d9e76144386b142e5075cb4879b511f
Maitreyee2095 <maitreyee.rao@gmail.com>
2021-04-11 13:33:38 -0600

    module added

--- base_external/package/server-socket/server-scoket.mk
+++ base_external/package/server-socket/server-scoket.mk
@@ -1,28 +1,27 @@
 
 ##############################################################
 #
-# AESD-ASSIGNMENTS
+# Server-socket
 #
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
+SERVER_SOCKET_VERSION = 8a8fb58658e2854693585380cb44529fb1c32839 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
-AESD_ASSIGNMENTS_SITE_METHOD = git
-AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+SERVER_SOCKET_SITE = git@github.com:cu-ecen-5013/assignment-3-Maitreyee2095.git
+SERVER_SOCKET_SITE_METHOD = git
+SERVER_SOCKET_GIT_SUBMODULES = YES
 
-define AESD_ASSIGNMENTS_BUILD_CMDS
+define SERVER_SOCKET_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
 endef
 
 # TODO add your writer, finder and tester utilities/scripts to the installation steps below
-define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/usr/bin/conf/
-	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/usr/bin/conf/
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+define SERVER_SOCKET_INSTALL_TARGET_CMDS
+
+	$(INSTALL) -m 0755 $(@D)/socketserver $(TARGET_DIR)/bin
 endef
 
 $(eval $(generic-package))
