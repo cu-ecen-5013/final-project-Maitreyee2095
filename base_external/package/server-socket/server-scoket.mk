5393b7805a38e9a0d6a883565043e5432e017e70
Maitreyee2095 <maitreyee.rao@gmail.com>
2021-04-18 15:11:25 -0600

    changes

--- base_external/package/server-socket/server-scoket.mk
+++ base_external/package/server-socket/server-scoket.mk
@@ -2,11 +2,11 @@
 ##############################################################
 #
 # Server-socket
-#
+#$(INSTALL) -m 0755 $(@D)/gpio $(TARGET_DIR)/bin
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-SERVER_SOCKET_VERSION = e3ba854664c083ac6dfa73d7adb0118d05bb5a06 
+SERVER_SOCKET_VERSION = ac5bf7dcd983da3f0f39b4319729e6371263fd93 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -21,7 +21,9 @@ endef
 # TODO add your writer, finder and tester utilities/scripts to the installation steps below
 define SERVER_SOCKET_INSTALL_TARGET_CMDS
 
-	$(INSTALL) -m 0755 $(@D)/gpio $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/lcd $(TARGET_DIR)/usr/bin
+
+
 endef
 
 $(eval $(generic-package))
